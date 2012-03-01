8926af9161aa57712e0badb833a8125cfbf66a5b
1n7erface7h1s <1n7erface7h1s@gmail.com>
2012-03-01 17:42:29 -0500

    Update p14.java

--- p14.java
+++ p14.java
@@ -1,5 +1,10 @@
-
-
+ /**
+  * @author 1n7erface7h1s 
+  * @email 1n7erface7h1s@gmail.com
+  * 
+  * I affirm that this program is entirely my own work and none of it is the work of any other person.
+  */ 
+  
 public class p14 {
 
    
