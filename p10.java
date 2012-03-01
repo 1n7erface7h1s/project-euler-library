6efa77757bf48a33fcdf2de8cfcb0144a28348e4
1n7erface7h1s <1n7erface7h1s@gmail.com>
2012-03-01 17:41:50 -0500

    Update p10.java

--- p10.java
+++ p10.java
@@ -1,3 +1,9 @@
+ /**
+  * @author 1n7erface7h1s 
+  * @email 1n7erface7h1s@gmail.com
+  * 
+  * I affirm that this program is entirely my own work and none of it is the work of any other person.
+  */ 
 import java.util.*;
 
 public class p10 {
